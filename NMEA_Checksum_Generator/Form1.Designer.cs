7b5325992822dfcf719987aa115bd36306557e11
netravler <pheintz@hotmail.com>
2017-04-28 14:11:54 -0500

    Add files via upload

---
+++ NMEA_Checksum_Generator/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace NMEA_Checksum_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toChecksum = new System.Windows.Forms.TextBox();
+            this.checksumD = new System.Windows.Forms.TextBox();
+            this.generateChecksum = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "String to convert Include $ & *";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Checksum";
+            // 
+            // toChecksum
+            // 
+            this.toChecksum.Location = new System.Drawing.Point(61, 67);
+            this.toChecksum.Name = "toChecksum";
+            this.toChecksum.Size = new System.Drawing.Size(217, 22);
+            this.toChecksum.TabIndex = 2;
+            // 
+            // checksumD
+            // 
+            this.checksumD.Location = new System.Drawing.Point(61, 155);
+            this.checksumD.Name = "checksumD";
+            this.checksumD.Size = new System.Drawing.Size(100, 22);
+            this.checksumD.TabIndex = 3;
+            // 
+            // generateChecksum
+            // 
+            this.generateChecksum.Location = new System.Drawing.Point(331, 80);
+            this.generateChecksum.Name = "generateChecksum";
+            this.generateChecksum.Size = new System.Drawing.Size(86, 23);
+            this.generateChecksum.TabIndex = 4;
+            this.generateChecksum.Text = "Generate";
+            this.generateChecksum.UseVisualStyleBackColor = true;
+            this.generateChecksum.Click += new System.EventHandler(this.generateChecksum_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 253);
+            this.Controls.Add(this.generateChecksum);
+            this.Controls.Add(this.checksumD);
+            this.Controls.Add(this.toChecksum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "NMEA Checksum";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox toChecksum;
+        private System.Windows.Forms.TextBox checksumD;
+        private System.Windows.Forms.Button generateChecksum;
+    }
+}
+
---
+++ NMEA_Checksum_Generator/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NMEA_Checksum_Generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generateChecksum_Click(object sender, EventArgs e)
+        {
+            checksumD.Text = getChecksum(toChecksum.ToString());
+        }
+        private static string getChecksum(string sentence)
+        {
+            //Start with first Item
+            int checksum = Convert.ToByte(sentence[sentence.IndexOf('$') + 1]);
+            // Loop through all chars to get a checksum
+            for (int i = sentence.IndexOf('$') + 2; i < sentence.IndexOf('*'); i++)
+            {
+                // No. XOR the checksum with this character's value
+                checksum ^= Convert.ToByte(sentence[i]);
+            }
+            // Return the checksum formatted as a two-character hexadecimal
+            return checksum.ToString("X2").ToUpper();
+        }
+
+        private static string getChecksum2(string stringToCalculateTheChecksumOver)
+        {
+            int checksum = 0;
+            for (int i = 0; i < stringToCalculateTheChecksumOver.Length; i++)
+            {
+                checksum ^= Convert.ToByte(stringToCalculateTheChecksumOver[i]);
+            }
+            // Return the checksum formatted as a two-character hexadecimal
+            return checksum.ToString("X2");
+        }
+    }
+}
